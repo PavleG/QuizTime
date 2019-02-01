c29ecc8a9f8a84c6d5c0c6663f35a0dd57821bf2
PavleG <pavlemgavrilovic@gmail.com>
2019-02-01 09:44:33 +0100

    ajax try

--- QuizTime/wwwroot/js/quizMaster.js
+++ QuizTime/wwwroot/js/quizMaster.js
@@ -3,14 +3,24 @@
 var connection = new signalR.HubConnectionBuilder().withUrl("/quizHub").build();
 
 connection.on("DisplayPlayerName", function (playerName) {
-    var div = document.createElement("div");
-    var name = document.createElement("span");
-    name.textContent = playerName;
-    var btn = document.createElement("button");
-    btn.textContent = "Remove";
-    div.appendChild(name);
-    div.appendChild(btn);
-    document.getElementById("playersList").appendChild(div);
+    // var div = document.createElement("div");
+    // var name = document.createElement("span");
+    // name.textContent = playerName;
+    // var btn = document.createElement("button");
+    // btn.textContent = "Remove";
+    // div.appendChild(name);
+    // div.appendChild(btn);
+    // document.getElementById("playersList").appendChild(div);
+        $.ajax({
+            url: '/Quiz/DisplayPlayer',
+            data: {'name': playerName},
+            success: function (response){
+                $("#players").html(response);
+            },
+            error: function () {
+                alert("error occured");
+            }
+        });
 });
 connection.start().catch(function (err) {
     return console.error(err.toString());
