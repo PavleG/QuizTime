fbbeb8d7bc677c70c722cc99582ac0c9e920e8d3
PavleG <pavlemgavrilovic@gmail.com>
2019-03-22 14:57:16 +0100

    Added DbSet of quizzes

--- QuizTime/Models/AppDbContext.cs
+++ QuizTime/Models/AppDbContext.cs
@@ -6,5 +6,6 @@ namespace QuizTime.Models{
     public class AppDbContext : DbContext{
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options){}
+        public DbSet<QuizModel> ListOfQuizzes { get; set;}
     }
 }
