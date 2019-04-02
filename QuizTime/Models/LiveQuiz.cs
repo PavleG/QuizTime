71919119644db0e2252a8365a44ed898f6f44248
PavleG <pavlemgavrilovic@gmail.com>
2019-04-02 09:51:39 +0200

    Added a model for an active quiz

---
+++ QuizTime/Models/LiveQuiz.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuizTime.Models {
+    // LiveQuiz class is used for an ongoing quiz (a quiz in progress)
+    // It holds the quiz that the quiz master has selected, the quiz master's information
+    // and the information about players that are participating
+    public class LiveQuiz{
+        private List<Player> Participants = new List<Player>();
+        public IEnumerable<Player> Players => Participants;
+        public User Host { get; set; }
+        public QuizModel Quiz { get; set; }
+        public bool AddPlayerToQuiz(string NewUserName){
+            Player player = Players.Where(p => p.UserName == NewUserName).FirstOrDefault();
+            if (player == null){
+                Participants.Add(new Player{
+                    UserName = NewUserName, 
+                    Points = 0
+                    });
+                return true;
+            }
+            return false;
+        }
+        public void RemovePlayerFromQuiz(string UserName){
+            Participants.RemoveAll(p => p.UserName == UserName);
+        }
+    }
+
+    public class Player
+    {
+        public string UserName { get; set; }
+        public int Points { get; set; }
+    }
+}
